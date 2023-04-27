bc604463572551090fbc7da591d42b291ab848ff
Rashidnashtar <Rashidmohamadnashtar2002@gmail.com>
2023-04-28 00:01:09 +0300

    fixing styled

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,25 +1,32 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection, Img } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection, Img } from "./HeroStyles";
 
 const Hero = (props) => (
   <>
-    <Section row nopadding>
+    <Section row nopadding lg_col>
       <LeftSection>
         <SectionTitle main center nopadding>
           Hussain Riad Khallouf
         </SectionTitle>
         <SectionText>
-        An AI Engineer and Backend Developer with a passion for learning and achievement, Outstanding time management and organizational abilities, eager to be challenged to grow and improve my skills.
+          An AI Engineer and Backend Developer with a passion for learning and
+          achievement, Outstanding time management and organizational abilities,
+          eager to be challenged to grow and improve my skills.
         </SectionText>
-        <Link  href="#about"><Button >Learn More</Button></Link>
+        <Link href="#about">
+          <Button>Learn More</Button>
+        </Link>
       </LeftSection>
+      <Img src="/images/picofme.png" />
     </Section>
-    <Img src="/images/picofme.png"/>
-
   </>
 );
 
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,14 +1,40 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Img = styled.img`
-  position: absolute;
-  width:350px;
-  height:350px;
-  top: 100px;
-  right: 50px;
+  /* position: absolute; */
+  width: 350px;
+  height: 350px;
+  margin-left: 5rem;
+  position: relative;
+
+  @media ${(props) => props.theme.breakpoints.xl} {
+    width: 250px;
+    height: 250px;
+  }
+  @media ${(props) => props.theme.breakpoints.lg} {
+    right: 50%;
+    transform: translateX(50%);
+    margin-bottom: 4rem;
+  }
+  @media ${(props) => props.theme.breakpoints.md} {
+    transform: translateX(0);
+    right: 0;
+    margin: 0 auto;
+    width: 200px;
+    height: 200px;
+  }
+
+  @media ${(props) => props.theme.breakpoints.sm} {
+    width: 150px;
+    height: 150px;
+  }
   object-fit: cover;
-  overflow: hidden;
-`
+  /* @media screen and (max-width: 1200px) {
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+  } */
+`;
 
 export const LeftSection = styled.div`
   width: 100%;
